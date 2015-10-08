3369eca088e4c11f9ec52f44ee8ca204fe9ad5f6
andy-dev <andres.macedo@gmail.com>
2015-10-08 16:18:04 -0700

    add some examples using map filter

--- asyncNetflix/a.js
+++ asyncNetflix/a.js
@@ -1,1 +1,30 @@
-a.js
+var myForFunct = function(){
+  var names = ["andy", "john", "cayden", "paul"], counter;
+
+  for(counter=0; counter < names.length; counter++){
+    console.log(names[counter]);
+  }
+}
+
+myForFunct();
+
+var myForEachFunct = function(){
+  var names = ["andy", "john", "cayden", "paul"]
+
+  names.forEach(function(name){
+    console.log(name);
+  });
+}
+
+myForEachFunct();
+
+//my own map
+// how map works
+var myMap = function(projectionFunction){
+  var results = [];
+  this.forEach(function(itemInArray){
+    results.push(projectionFunction(itemInArray));
+  })
+  return results;
+}
+
